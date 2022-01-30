c2817158f1ae496717f7ef7ad0d8ade348457fa7
ayush <ayush@LAPTOP-DV4F95LB>
2022-01-30 16:25:26 +0530

    Add project files.

---
+++ TWOVSTEN/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int x, num = 0;
+		cin >> x;
+		if (x % 10 == 0)
+		{
+			cout << 0 << endl;
+		}
+		else if (x % 5 == 0)
+		{
+			cout << 1 << endl;
+		}
+		else
+			cout << -1 << endl;
+	}
+return 0;
+}
